572bff77de97d5461edc48c1f61fda241ef67b05
Федор <143493691+KwayloD@users.noreply.github.com>
2023-12-18 16:02:07 +0400

    Parametrize.py

---
+++ parametrize.py
@@ -0,0 +1,66 @@
+import pytest
+
+def python_string_slicer(str):
+   if len(str) < 50 or "python" in str:
+       return str
+   else:
+       return str[0:50]
+
+@pytest.fixture(scope="function", params=[
+   ("Короткая строка", "Короткая строка"),
+   ("Длинная строка, не то чтобы прям очень длинная, но достаточно для нашего теста, и в ней нет названия языка"
+    , "Длинная строка, не то чтобы прям очень длинная, но"),
+   ("Короткая строка со словом python", "Короткая строка со словом python"),
+   ("Длинная строка, нам достаточно будет для проверки, и в ней есть слово python"
+    , "Длинная строка, нам достаточно будет для проверки, и в ней есть слово python")
+])
+def param_fun(request):
+   return request.param
+
+def test_python_string_slicer(param_fun):
+   (input, expected_output) = param_fun
+   result = python_string_slicer(input)
+   print ("Входная строка: {0}\nВыходная строка: {1}\nОжидаемое значение: {2}".format(input, result, expected_output))
+   assert result == expected_output
+
+
+
+@pytest.mark.parametrize("x", [1, 2, 3], ids=["negative", "zero", "positive"])
+@pytest.mark.parametrize("y", [10, 11], ids=["3 digit", "4 digit"])
+def test_multiply_params(x, y):
+    print("x: {0}, y: {1}".format(x, y))
+    assert True
+
+def ids_x(val):
+    return "x=({0})".format(str(val))
+def ids_y(val):
+    return "y=({0})".format(str(val))
+
+
+
+@pytest.mark.parametrize("x", [-1, 0, 1], ids=ids_x)
+@pytest.mark.parametrize("y", [100, 1000], ids=ids_y)
+def test_multiply_params(x, y):
+    print("x: {0}, y: {1}".format(x, y))
+    assert True
+
+
+
+#27.1.4
+#Решим следующую задачу: напишите функцию, которая определяет, можно ли составить треугольник из трёх отрезков, длины которых передаются в функцию.
+#Например, вызов функции is_triangle(3, 4, 5) возвращает True, а вызов is_triangle(1, 4, 5) возвращает False. Напишите параметризованный тест на эту функцию,
+#используя фикстуру @pytest.mark.parametrize, который рассматривает все возможные варианты параметров:
+def is_triangle(a, b, c):
+    if a + b > c and b + c > a and a + c > b:
+        return True
+    else:
+        return False
+      
+@pytest.mark.parametrize("a, b, c, expected", [
+    (3, 4, 5, True),
+    (1, -4, 5, False),
+    (2, 2, 5, False),
+    (1, 0, 3, False)
+])
+def test_is_triangle(a, b, c, expected):
+    assert is_triangle(a, b, c) == expected
